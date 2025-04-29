3226daf27bdee1010041b355105307cd155551c6
goqkTk <jty_419@naver.com>
2025-04-29 22:28:49 +0900

    미디어쿼리 적용

--- app.py
+++ app.py
@@ -351,4 +351,4 @@ def delete_transaction(project_id, transaction_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
